0200f65f532191cefccee7267c2b0c59e892781d
CtheJester <cgrusendorf@student.cscc.edu>
2025-12-09 14:27:59 -0500

    Refactored Code into functions

--- cgrusendorf-#1.py
+++ cgrusendorf-#1.py
@@ -1,4 +1,8 @@
 """
+Unemployment Chart
+Breaks down a two colum OHUR file on unemployment.
+by Caleb Grusendorf
+12/9/2025
 """
 
 from pathlib import Path
@@ -6,31 +10,39 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-path = Path('OHUR.csv')
-lines = path.read_text(encoding='utf-8').splitlines()
+def CSV_extraction():
+    path = Path('OHUR.csv')
+    lines = path.read_text(encoding='utf-8').splitlines()
 
-reader = csv.reader(lines)
-header_row = next(reader)
-print(header_row)
-for index, col_title in enumerate(header_row):
-    print(f'{index}, {col_title}', end=" ")
+    reader = csv.reader(lines)
+    header_row = next(reader)
+    print(header_row)
+    for index, col_title in enumerate(header_row):
+        print(f'{index}, {col_title}', end=" ")
+    return reader
 
-date = []
-OHUR = []
+def csv_breakdown(reader):
+    date = []
+    OHUR = []
 
-for row in reader:
-    current_date = datetime.strptime(row[0],'%Y-%m-%d')
-    ohur = float(row[1])
-    date.append(current_date)
-    OHUR.append(ohur)
+    for row in reader:
+        current_date = datetime.strptime(row[0],'%Y-%m-%d')
+        ohur = float(row[1])
+        date.append(current_date)
+        OHUR.append(ohur)
+    return date,OHUR
 
+def create_graph(date, OHUR):
+    plt.style.use('dark_background')
+    figure, graph = plt.subplots()
 
+    graph.plot(date, OHUR, color='blue')
+    figure.autofmt_xdate()
+    graph.set_title('OHUR Numbers')
+    graph.set_ylabel('Unemployment Rate', fontsize=12)
+    graph.set_xlabel('Date')
+    plt.show()
 
-plt.style.use('dark_background')
-figure, graph = plt.subplots()
-
-graph.plot(date, OHUR, color='blue')
-figure.autofmt_xdate()
-graph.set_title('OHUR Numbers')
-graph.set_ylabel('Unemployment Rate', fontsize=12)
-plt.show()
+reader = CSV_extraction()
+date, OHUR = csv_breakdown(reader)
+create_graph(date, OHUR)
